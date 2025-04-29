19730efa01646dd4a77dd07328e497716f08b179
Anurag <anuragchakravarty4@gmail.com>
2025-04-29 13:55:24 +0530

    work done

--- push-and-run.bat
+++ push-and-run.bat
@@ -8,7 +8,7 @@ set STACK_NAME=my-app
 
 REM Build the image
 echo Building Docker image...
-docker build -t %DOCKER_USERNAME%/%IMAGE_NAME% .
+docker build -t %DOCKER_USERNAME%/%IMAGE_NAME% ./client
 
 REM Push the image to Docker Hub
 echo Pushing image to Docker Hub...
