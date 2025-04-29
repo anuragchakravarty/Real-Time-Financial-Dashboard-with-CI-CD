d6574f2a6cc94b3d36f7bf51c78d8c58003706ce
Anurag Chakravarty <120595824+anuragchakravarty@users.noreply.github.com>
2025-04-29 11:53:24 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import yfinance as yf
 import plotly.express as px
 
 # --- Title ---
-st.title('📈 Stock Dashboard 15')
+st.title('📈 Stock Dashboard 115')
 
 # --- Sidebar Inputs ---
 ticker = st.sidebar.text_input('Enter Stock Ticker (e.g., MSFT)', 'MSFT')
